4a0b99b68b0502e9b146b3164dfe9165b4ad9607
manish-yadav4 <manishy8572@gmail.com>
2025-09-09 21:33:31 +0530

    mmmmm

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const Navbar = () => {
     const[menu, openMenu] = useState(false);
-    const[showMenu, setShowmenu] = useState(true);
+    const[showMenu, setShowmenu] = useState(false);
   return (
     <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20'>
         <span className='text-xl font-bold tracking-wide'>Portfolio</span>
@@ -23,10 +23,13 @@ const Navbar = () => {
         </ul>
 
 {showMenu?(
-    <RiMenu2Line size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300 ' onClick={()=>
-        {openMenu(!menu), setShowmenu(!showMenu);}}/>
+    <RiCloseLine size={30} onClick={()=>{openMenu(false), setShowmenu(false)}} className='md:hidden absolute right-10 top-6 transition-all duration-300 '/>
+
 ):
-<RiCloseLine size={30} onClick={()=>openMenu(false)} className='md:hidden absolute right-10 top-6 transition-all duration-300 '/>
+
+
+    <RiMenu2Line size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300 ' onClick={()=>
+        {openMenu((p)=>!p), setShowmenu(true);}}/>
 }
         </nav>
   ) 
