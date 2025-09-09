6e25048193704009034813e91394013957693b5c
manish-yadav4 <manishy8572@gmail.com>
2025-09-09 21:27:09 +0530

    mmm

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -23,10 +23,10 @@ const Navbar = () => {
         </ul>
 
 {showMenu?(
-    <RiMenu2Line size={30} className='md:hidden absolute right-10top-6 transition-all duration-300 ' onClick={()=>
+    <RiMenu2Line size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300 ' onClick={()=>
         {openMenu(!menu), setShowmenu(!showMenu);}}/>
 ):
-<RiCloseLine size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300 '/>
+<RiCloseLine size={30} onClick={()=>openMenu(false)} className='md:hidden absolute right-10 top-6 transition-all duration-300 '/>
 }
         </nav>
   ) 
